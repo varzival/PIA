b058f51c70a2916eaccfc1e40c84b0903b5105fe
Vadim Goryainov <vg.uni@t-online.de>
2018-04-17 16:14:27 +0200

    pages and persistant saver

---
+++ Assets/scripts/PersistantSaver.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Security.Cryptography;
+using System.Text;
+using System;
+using UnityEngine;
+
+public class PersistantSaver {
+
+    public struct PlayerData
+    {
+        public int[] points;
+        public int[] stationOrder;
+    }
+
+    private static PlayerData playerData;
+
+    //Fisher Yates Shuffle
+    public static void Shuffle(int[] list)
+    {
+        int i = list.Length-1;
+        while (i >= 1)
+        {
+            int k = UnityEngine.Random.Range(0, i+1);
+            int value = list[k];
+            list[k] = list[i];
+            list[i] = value;
+            i--;
+        }
+    }
+
+    public static int[] Points
+    {
+        get { return playerData.points; }
+        set { playerData.points = value; savePoints(); saveHash(); }
+    }
+
+    public static int[] StationOrder
+    {
+        get { return playerData.stationOrder; }
+    }
+
+    public static int getPoints(int station)
+    {
+        return playerData.points[station];
+    }
+
+    public static void init()
+    {
+        playerData.points = new int[5];
+        playerData.stationOrder = new int[5];
+        loadAll();
+    }
+
+	public static void savePoints()
+    {
+        for (int station = 0; station < playerData.points.Length; station++)
+        {
+            PlayerPrefs.SetInt("s" + station, playerData.points[station]);
+        }
+    }
+
+    public static void saveAll()
+    {
+        savePoints();
+        saveStationOrder();
+        saveHash();
+    }
+
+    private static void saveStationOrder()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<playerData.stationOrder.Length; i++)
+        {
+            sb.Append(playerData.stationOrder[i] + ".");
+        }
+        sb.Remove(playerData.stationOrder.Length - 1, 1);
+        PlayerPrefs.SetString("so", sb.ToString());
+    }
+
+    public static void loadAll()
+    {
+        string hashStringRead = PlayerPrefs.GetString("h", "null");
+        if (hashStringRead.Equals("null"))
+        {
+            createNewSave();
+        }
+        else
+        {
+            for (int station = 0; station < playerData.points.Length; station++)
+            {
+                playerData.points[station] = PlayerPrefs.GetInt("s" + station, 0);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            string so_str = PlayerPrefs.GetString("so");
+            string[] so_split = so_str.Split('.');
+            try
+            {
+                for (int i = 0; i < playerData.stationOrder.Length; i++)
+                {
+                    playerData.stationOrder[i] = Int32.Parse(so_split[i]);
+                }
+            }
+            catch (System.Exception)
+            {
+                Debug.Log("Could not read station order.");
+                createNewSave();
+                return;
+            }
+
+            string hashStringGenerated = generateHashFromData();
+            if (!hashStringRead.Equals(hashStringGenerated))
+            {
+                Debug.Log("Save File corrupted!");
+                createNewSave();
+            }
+        }
+        
+    }
+
+    private static void createNewSave()
+    {
+        Debug.Log("Creating new save.");
+        for (int i = 0; i < playerData.points.Length; i++)
+            playerData.points[i] = 0;
+
+        //Randomize stations
+        for (int i = 0; i < playerData.stationOrder.Length; i++)
+        {
+            playerData.stationOrder[i] = i;
+        }
+        Shuffle(playerData.stationOrder);
+
+        saveAll();
+    }
+    /*
+    public void changePlayerPoints(int station, int points)
+    {
+        playerData.points[station] = points;
+        savePoints();
+    }
+    */
+
+    private static void saveHash()
+    {
+        string hashString = generateHashFromData();
+        PlayerPrefs.SetString("h", hashString);
+    }
+
+    private static string generateHashFromData()
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int station = 0; station < playerData.points.Length; station++)
+        {
+            sb.Append("s" + station + "_" + playerData.points[station]);
+        }
+        sb.Append("so_");
+        for (int i = 0; i < playerData.stationOrder.Length; i++)
+        {
+            sb.Append(playerData.points[i]+".");
+        }
+        sb.Remove(playerData.stationOrder.Length - 1, 1);
+
+        return generateHash(sb.ToString());
+    }
+
+    private static string generateHash(string data)
+    {
+        byte[] byteData = Encoding.ASCII.GetBytes(data);
+        SHA1 sha = new SHA1CryptoServiceProvider();
+        byte[] hash = sha.ComputeHash(byteData);
+        return Encoding.ASCII.GetString(hash);
+    }
+}
---
+++ Assets/scripts/severalPages.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class severalPages : MonoBehaviour {
+
+    public GameObject[] pages;
+    public GameObject prevButton;
+    public GameObject nextButton;
+    private int currentPage = 0;
+
+    public void next()
+    {
+        if (currentPage+1 >= pages.Length)
+        {
+            Debug.Log("Page overflow.");
+        }
+        else
+        {
+            pages[currentPage].SetActive(false);
+            pages[++currentPage].SetActive(true);
+            checkButtons();
+        }
+    }
+
+    public void previous()
+    {
+        if (currentPage - 1 < 0)
+        {
+            Debug.Log("Page underflow.");
+        }
+        else
+        {
+            pages[currentPage].SetActive(false);
+            pages[--currentPage].SetActive(true);
+            checkButtons();
+        }
+    }
+
+    void checkButtons()
+    {
+        if (currentPage == 0)
+        {
+            prevButton.SetActive(false);
+            nextButton.SetActive(true);
+        }
+        else if (currentPage == pages.Length - 1)
+        {
+            prevButton.SetActive(true);
+            nextButton.SetActive(false);
+        }
+        else
+        {
+            prevButton.SetActive(true);
+            nextButton.SetActive(true);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        pages[0].SetActive(true);
+        prevButton.SetActive(false);
+        nextButton.SetActive(true);
+		for (int i = 1; i<pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
